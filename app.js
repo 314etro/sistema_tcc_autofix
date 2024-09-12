c53c4371cfa5cbfa40f9fb01ea2f6c2a0b639313
314etro <pietro.cunhadev@gmail.com>
2024-09-12 09:03:35 -0300

    Pietro- tcc 12/09

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 //rotas
+//rotas
 app.get('/', (req, res) => {
     res.render('index1');
 });
@@ -45,8 +46,17 @@ app.get('/home_mecanico', (req, res) => {
     res.render('home_mecanico');
 });
 
-app.get('/clientes_mecanico', (req, res) => {
-    res.render('clientes_mecanico');
+
+app.get('/inspecao_manutencao_pendente', (req, res) => {
+    res.render('inspecao_manutencao_pendente');
+});
+
+app.get('/manutencoes_andamento', (req, res) => {
+    res.render('manutencoes_andamento');
+});
+
+app.get('/manutencoes_finalizadas_mecanico', (req, res) => {
+    res.render('manutencoes_finalizadas_mecanico');
 });
 
 app.get('/home_adm', (req, res) => {
@@ -56,6 +66,25 @@ app.get('/home_adm', (req, res) => {
 app.get('/aprovar_entrada_cliente', (req, res) => {
     res.render('aprovar_entrada_cliente');
 });
+
+app.get('/aprovar_orcamento_cliente', (req, res) => {
+    res.render('aprovar_orcamento_cliente');
+});
+
+app.get('/funcionarios_adm', (req, res) => {
+    res.render('funcionarios_adm');
+});
+
+app.get('/orcamentos_adm', (req, res) => {
+    res.render('orcamentos_adm');
+});
+
+app.get('/fornecedor_adm', (req, res) => {
+    res.render('fornecedor_adm');
+});
+
+
+
 
 // Login
 app.post('/loginMecanico', (req,res)=>{
@@ -141,3 +170,29 @@ app.post('/loginCliente', (req,res)=>{
         }
     })
 }); 
+
+app.get('/inspecao_manutencao', (req, res) => {
+    const clientes = [
+        { nome: "João Aparecido", modelo: "Honda - Civic type R", placa: "XXX-0000" },
+        { nome: "Maria Silva", modelo: "Toyota - Corolla", placa: "YYY-1111" },
+        { nome: "Julia Santos", modelo: "Ford - Focus", placa: "ZZZ-2222" },
+        { nome: "Estela Clara", modelo: "Chevrolet - Onix", placa: "AAA-3333" }
+    ];
+
+    res.render('inspecao_manutencao', { clientes });
+});
+
+
+app.get('/clientes_mecanico', (req,res)=>{
+    db.query('SELECT c.nome AS nome_cliente, c.telefone AS telefone, c.email AS email, c.cpf_cliente AS cpf_cliente, v.placa AS placa_veículo FROM cliente c JOIN veiculo v ON c.cpf_cliente = v.cpfcliente;', (error,results) =>{
+        if(error){
+            console.log('nao foi possivel ver os clientes', error);
+  
+            
+        }else{
+
+            res.render('clientes_mecanico', {clientes: results})
+        }
+    })
+});
+
